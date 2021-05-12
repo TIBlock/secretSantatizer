fc064b160abd7c0e272ee4129a4e96c1c9b432ef
TIBlock <taylorblocker@gmail.com>
2021-05-11 20:41:07 -0500

    comitting changes to user model and app.py

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from flask import Flask
 from flask_restful import Api
-# from flask_jwt_extended import JWTManager
+from flask_jwt import JWT
 
-from db import db
+from resources.users import UserRegister, User
 
-# complete revamp of codebase starts here
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.secret_key = 'taylor'
+api = Api(app)
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
-app = Flask(__name__)   
-
-
+# jwt = JWT(app, authenticate, identity)
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    from db import db
+    db.init_app(app)
+    app.run(debug=True)  # important to mention debug=True
